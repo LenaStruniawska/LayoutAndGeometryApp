7cdb4d8d4b7c0f8b917fb33d9528a3e5eb687370
Magdalena Struniawska <mag.sok@onet.pl>
2022-05-26 07:51:40 +0200

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Slawek on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
